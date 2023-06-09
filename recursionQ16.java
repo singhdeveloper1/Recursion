c79d0ca662c6ace013fb8c914813a9c7db7e49ef
Priyanshu Singh <118466432+singhdeveloper1@users.noreply.github.com>
2023-06-09 21:23:12 +0530

    Add files via upload

---
+++ recursionQ16.java
@@ -0,0 +1,35 @@
+public class recursionQ16 {
+
+//      *  
+//     * * 
+//    * * *
+//   * * * *
+
+    static void printspace(int space){
+        if(space==0){
+            return;
+        }
+        System.out.print(" ");
+        printspace(space-1);
+
+    }
+
+static void printstar(int col){
+    if(col==0){
+        return;
+    }
+    System.out.print("* ");
+    printstar(col-1);
+}
+static void printpattern(int row,int col){
+    if(row==0){
+        return;
+    }
+    printspace(row);
+    printstar(col);
+    System.out.println();
+    printpattern(row-1,col+1);
+}
+public static void main(String[] args) {
+    printpattern(5,0);    }
+}
